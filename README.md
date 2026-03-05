cab48497dd95538193ea5dab0df3dbea74e6fb4f
tushalbarvaliya <tushal.barvaliya@seaflux.tech>
2026-03-05 11:18:38 +0530

    chore : install tailwind,reata-router-dom,shadcn,tanstack

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Legal management App
 
+## Installation in React All
+
+- Tailwind Fro Style.
+- React Router Dom For Navigation.
+- Shadcn for UI Components
+- tanstack for server side state management.
+
+
+## File Structure
+
+- types folder for : define the type to data.
+- assets folder for : assets 
+- 
---
+++ src/main.tsx
@@ -0,0 +1,24 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import "./index.css";
+import App from "./App.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </StrictMode>,
+);
