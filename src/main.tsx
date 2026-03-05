f43a713eb8b8c5c1824367b980b14c69be3dbec1
tushalbarvaliya <tushal.barvaliya@seaflux.tech>
2026-03-05 12:05:12 +0530

    feat : persist store setup.

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,8 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
+import { persistor } from "./store/store.tsx";
+import { PersistGate } from "redux-persist/integration/react";
 import "./index.css";
 
 import LoginPage from "./pages/LoginPage.tsx";
@@ -11,7 +12,6 @@ import SignUpPage from "./pages/SignUpPage.tsx";
 import LogOutPage from "./pages/LogOutPage.tsx";
 import ProfilePage from "./pages/ProfilePage.tsx";
 import App from "./App.tsx";
-
 
 const queryClient = new QueryClient();
 
@@ -40,8 +40,10 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
+    <PersistGate loading={null} persistor={persistor}>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </PersistGate>
   </StrictMode>,
 );
---
+++ src/store/store.tsx
@@ -0,0 +1,25 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ['counter'],
+};
+
+const rootReducer = combineReducers({
+  counter: {},
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }),
+});
+
+export const persistor = persistStore(store);
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
