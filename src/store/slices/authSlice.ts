f2e36f16a316d547027398b4bbb6fb1e39d6d378
tushalbarvaliya <tushal.barvaliya@seaflux.tech>
2026-03-09 17:18:49 +0530

    feat: auth token slice add to store

---
+++ src/store/slices/authSlice.ts
@@ -0,0 +1,26 @@
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+
+interface authState {
+  token: null | string;
+}
+
+const initialState: authState = {
+  token: null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setToken: (state, action: PayloadAction<{ token: string }>) => {
+      state.token = action.payload.token;
+    },
+    removeToken: (state) => {
+      state.token = null;
+    },
+  },
+});
+
+export const { setToken, removeToken } = authSlice.actions;
+
+export default authSlice.reducer;
