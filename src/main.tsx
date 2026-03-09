d3c02c1c350623cb867348358799d814cce3afbd
tushalbarvaliya <tushal.barvaliya@seaflux.tech>
2026-03-09 17:19:29 +0530

    fix: route protection fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter } from "react-router";
+import { createBrowserRouter, redirect } from "react-router";
 import { RouterProvider } from "react-router/dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { PersistGate } from "redux-persist/integration/react";
@@ -11,62 +11,78 @@ import "./index.css";
 import { persistor, store } from "./store/store.tsx";
 import LoginPage from "./pages/LoginPage.tsx";
 import SignUpPage from "./pages/SignUpPage.tsx";
-import LogOutPage from "./pages/LogOutPage.tsx";
 import ProfilePage from "./pages/ProfilePage.tsx";
 import HomePage from "./pages/HomePage.tsx";
+import ProtectedRoute from "./routes/ProtectedRoute.tsx";
+import PublicRoute from "./routes/PublicRoute.tsx";
+import { removeToken } from "./store/slices/authSlice.ts";
 
 const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <HomePage />,
+    element: <ProtectedRoute />,
     children: [
       {
-        path: "profile",
-        element: <ProfilePage></ProfilePage>,
-      },
-      {
-        path: "ticket",
-        element: <ProfilePage></ProfilePage>,
-      },
-      {
-        path: "task",
-        element: <ProfilePage></ProfilePage>,
-      },
-      {
-        path: "docs",
-        element: <ProfilePage></ProfilePage>,
-      },
-      {
-        path: "cases",
-        element: <ProfilePage></ProfilePage>,
-      },
-      {
-        path: "client",
-        element: <ProfilePage></ProfilePage>,
-      },
-      {
-        path: "privacy-policy",
-        element: <ProfilePage></ProfilePage>,
-      },
-      {
-        path: "terms-and-conditions",
-        element: <ProfilePage></ProfilePage>,
+        path: "/",
+        element: <HomePage />,
+        children: [
+          {
+            path: "profile",
+            element: <ProfilePage></ProfilePage>,
+          },
+          {
+            path: "ticket",
+            element: <ProfilePage></ProfilePage>,
+          },
+          {
+            path: "task",
+            element: <ProfilePage></ProfilePage>,
+          },
+          {
+            path: "docs",
+            element: <ProfilePage></ProfilePage>,
+          },
+          {
+            path: "cases",
+            element: <ProfilePage></ProfilePage>,
+          },
+          {
+            path: "client",
+            element: <ProfilePage></ProfilePage>,
+          },
+          {
+            path: "privacy-policy",
+            element: <ProfilePage></ProfilePage>,
+          },
+          {
+            path: "terms-and-conditions",
+            element: <ProfilePage></ProfilePage>,
+          },
+        ],
       },
     ],
   },
   {
-    path: "/login",
-    element: <LoginPage />,
-  },
-  {
-    path: "/signUp",
-    element: <SignUpPage></SignUpPage>,
+    element: <PublicRoute />,
+    children: [
+      {
+        path: "/login",
+        element: <LoginPage />,
+      },
+      {
+        path: "/signUp",
+        element: <SignUpPage></SignUpPage>,
+      },
+    ],
   },
   {
     path: "/logout",
-    element: <LogOutPage></LogOutPage>,
+    loader: () => {
+      store.dispatch(removeToken());
+      persistor.purge();
+      return redirect("/login");
+    },
   },
 ]);
 
