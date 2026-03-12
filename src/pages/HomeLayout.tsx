f180de085a38076457b046d0e900a86f5e7b583c
tushalbarvaliya <tushal.barvaliya@seaflux.tech>
2026-03-12 16:47:42 +0530

    fix: homepage layout fix.

--- src/pages/HomeLayout.tsx
+++ src/pages/HomeLayout.tsx
@@ -1,21 +1,23 @@
 import Header from "@/components/Header";
 import Sidebar from "@/components/Sidebar";
 import { Outlet } from "react-router";
-
-// [&::-webkit-scrollbar]:w-2
+import { Toaster } from "sonner";
 const HomeLayout = () => {
   return (
-    <div className="h-screen w-full">
-      <Header />
-      <div className="flex mt-4 gap-2 items-center">
-        <div className="rounded-xl hidden lg:block  bg-white mx-2 p-4 shadow-[0px_10px_1px_rgba(221,221,221,1),0_10px_20px_rgba(204,204,204,1)]">
-          <Sidebar />
+    <>
+      <Toaster position="bottom-right" richColors />
+      <div className="h-screen w-full">
+        <Header />
+        <div className="flex mt-4 gap-2 items-center">
+          <div className="rounded-xl hidden lg:block  bg-white mx-2 p-4 shadow-[0px_10px_1px_rgba(221,221,221,1),0_10px_20px_rgba(204,204,204,1)]">
+            <Sidebar />
+          </div>
+          <main className="flex-1 bg-transparent  mr-2 overflow-y-scroll h-[85vh]  no-scrollbar ">
+            <Outlet />
+          </main>
         </div>
-        <main className="flex-1 bg-transparent  mr-2 overflow-y-scroll h-[85vh]  no-scrollbar ">
-          <Outlet />
-        </main>
       </div>
-    </div>
+    </>
   );
 };
 
