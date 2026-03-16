04c1b2767b8b61b880dc033639ecb4de902398bc
tushalbarvaliya <tushal.barvaliya@seaflux.tech>
2026-03-16 13:31:04 +0530

    feat:client page create

---
+++ src/types/clientType.ts
@@ -0,0 +1,14 @@
+export type ClientProps = {
+  firstName: string;
+  lastName: string;
+  mobileNumber: string;
+  otherNumber: string;
+  occupation: string;
+  gender: string;
+  address: string;
+  notes: string;
+  _id: string;
+  email: string;
+  isBlock: boolean;
+  isDelete: boolean;
+};
