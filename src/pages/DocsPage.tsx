13f2f7f2ec4e22209501684827dacecaa4df0556
tushalbarvaliya <tushal.barvaliya@seaflux.tech>
2026-03-20 09:21:04 +0530

    fix: docs page error handel and disable search and filter if error occurred

--- src/pages/DocsPage.tsx
+++ src/pages/DocsPage.tsx
@@ -1,151 +1,3 @@
-// import { useMemo, useState } from "react";
-// import DocsCard from "@/components/DocsModels/DocsCard";
-// import DocsHeader from "@/components/DocsModels/DocsHeader";
-// import type { DocumentData } from "@/types/docsType";
-// import DocsCardSkeleton from "@/components/DocsModels/DocsCardSkeleton";
-// import { useQuery } from "@tanstack/react-query";
-// import { getAllDocs } from "@/api/docsAPI";
-
-// const DocsPage = () => {
-//   const [search, setSearch] = useState("");
-//   const [fileType, setFileType] = useState("all");
-//   const [caseId, setCaseId] = useState("all");
-//   const [clientId, setClientId] = useState("all");
-
-//   const { data: documents, isLoading } = useQuery({
-//     queryKey: ["docs"],
-//     queryFn: getAllDocs,
-//   });
-
-//   const filteredDocs = useMemo(() => {
-//     return documents?.filter((doc: DocumentData) => {
-//       const matchesSearch =
-//         doc.title.toLowerCase().includes(search.toLowerCase()) ||
-//         doc.description.toLowerCase().includes(search.toLowerCase()) ||
-//         doc.caseId.toLowerCase().includes(search.toLowerCase());
-
-//       const matchesFileType = fileType === "all" || doc.fileType === fileType;
-
-//       const matchesCase = caseId === "all" || doc.caseId === caseId;
-
-//       return matchesSearch && matchesFileType && matchesCase;
-//     });
-//   }, [search, fileType, caseId, documents]);
-
-//   return (
-//     <>
-//       <DocsHeader />
-
-//       <div className="rounded-2xl border border-zinc-200 bg-white p-4 shadow-soft sm:p-6 mt-4">
-//         {/* Header */}
-//         <div className="mb-3 flex flex-wrap items-center justify-between gap-2">
-//           <div>
-//             <h2 className="text-lg font-semibold text-zinc-900">
-//               Select Documents
-//             </h2>
-//             <p className="mt-1 text-sm text-zinc-600">
-//               Choose a Document card to load related documents.
-//             </p>
-//           </div>
-
-//           <p className="text-xs uppercase bg-black text-white flex items-center font-semibold p-2 rounded-full font-mono">
-//             {filteredDocs?.length}
-//           </p>
-//         </div>
-
-//         <div className="mb-3 flex gap-4 flex-wrap">
-//           {/* Search */}
-//           <div className="relative flex-1 ">
-//             <img
-//               src="/search.svg"
-//               alt="search"
-//               className="pointer-events-none absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2"
-//             />
-//             <input
-//               type="search"
-//               placeholder="Search documents here..."
-//               value={search}
-//               onChange={(e) => setSearch(e.target.value)}
-//               className="w-full rounded-xl border border-zinc-200 py-2.5 pl-9 pr-3 text-sm outline-none focus:border-zinc-400 focus:ring-2 focus:ring-zinc-100"
-//             />
-//           </div>
-
-//           {/* File Type */}
-//           <select
-//             value={fileType}
-//             onChange={(e) => setFileType(e.target.value)}
-//             className="rounded-xl border border-zinc-200 px-3 py-2.5 text-sm"
-//           >
-//             <option value="all">All Type</option>
-//             {[
-//               ...new Set(documents.map((item: DocumentData) => item.fileType)),
-//             ].map((item: DocumentData) => (
-//               <option key={item._id} value={item.fileType}>
-//                 {item.fileType}
-//               </option>
-//             ))}
-//           </select>
-
-//           {/* Case */}
-//           <select
-//             value={caseId}
-//             onChange={(e) => setCaseId(e.target.value)}
-//             className="rounded-xl border border-zinc-200 px-3 py-2.5 text-sm"
-//           >
-//             <option value="all">All Cases</option>
-//             {[...new Set(documents.map((item) => item.caseId))].map((item) => (
-//               <option key={item} value={item}>
-//                 {item}
-//               </option>
-//             ))}
-//           </select>
-
-//           {/* Client */}
-//           <select
-//             value={clientId}
-//             onChange={(e) => setClientId(e.target.value)}
-//             className="rounded-xl border border-zinc-200 px-3 py-2.5 text-sm"
-//           >
-//             <option value="all">All Clients</option>
-//             {[...new Set(documents.map((item) => item.clientId))].map(
-//               (item) => (
-//                 <option key={item} value={item}>
-//                   {item}
-//                 </option>
-//               ),
-//             )}
-//           </select>
-//         </div>
-
-//         {/* List */}
-//         <div className="flex w-full gap-4 flex-col">
-//           {isLoading && (
-//             <>
-//               <DocsCardSkeleton />
-//               <DocsCardSkeleton />
-//               <DocsCardSkeleton />
-//               <DocsCardSkeleton />
-//             </>
-//           )}
-//           {filteredDocs.length > 0 ? (
-//             filteredDocs.map((items: DocumentData) => (
-//               <>
-//                 <DocsCard {...items} key={items._id} />
-//                 {/* <DocsCardSkeleton /> */}
-//               </>
-//             ))
-//           ) : (
-//             <p className="text-center text-sm text-zinc-500 py-6">
-//               No documents found
-//             </p>
-//           )}
-//         </div>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default DocsPage;
 import { useMemo, useState } from "react";
 import DocsCard from "@/components/DocsModels/DocsCard";
 import DocsHeader from "@/components/DocsModels/DocsHeader";
@@ -153,23 +5,23 @@ import type { DocumentData } from "@/types/docsType";
 import DocsCardSkeleton from "@/components/DocsModels/DocsCardSkeleton";
 import { useQuery } from "@tanstack/react-query";
 import { getAllDocs } from "@/api/docsAPI";
+import ErrorMessage from "@/components/ErrorMessage";
+import { Spinner } from "@/components/ui/spinner";
 
 const DocsPage = () => {
   const [search, setSearch] = useState("");
   const [fileType, setFileType] = useState("all");
   const [caseId, setCaseId] = useState("all");
 
-  const { data, isLoading } = useQuery({
+  const { data, isLoading, isError } = useQuery({
     queryKey: ["docs"],
     queryFn: getAllDocs,
   });
 
-  
   const documents: DocumentData[] = useMemo(() => {
     return Array.isArray(data) ? data : data?.data || [];
   }, [data]);
 
-  
   const filteredDocs = useMemo(() => {
     return documents.filter((doc) => {
       const matchesSearch =
@@ -202,7 +54,9 @@ const DocsPage = () => {
           </div>
 
           <p className="text-xs uppercase bg-black text-white flex items-center font-semibold p-2 rounded-full font-mono">
-            {filteredDocs.length}
+            {isLoading && <Spinner />}
+            {isError && 0}
+            {!isLoading && !isError && filteredDocs.length}
           </p>
         </div>
 
@@ -221,6 +75,7 @@ const DocsPage = () => {
               value={search}
               onChange={(e) => setSearch(e.target.value)}
               className="w-full rounded-xl border border-zinc-200 py-2.5 pl-9 pr-3 text-sm"
+              disabled={isLoading || isError}
             />
           </div>
 
@@ -229,6 +84,7 @@ const DocsPage = () => {
             value={fileType}
             onChange={(e) => setFileType(e.target.value)}
             className="rounded-xl border border-zinc-200 px-3 py-2.5 text-sm"
+            disabled={isLoading || isError}
           >
             <option value="all">All Type</option>
             {[...new Set(documents.map((item) => item.fileType))].map(
@@ -245,6 +101,7 @@ const DocsPage = () => {
             value={caseId}
             onChange={(e) => setCaseId(e.target.value)}
             className="rounded-xl border border-zinc-200 px-3 py-2.5 text-sm"
+            disabled={isLoading || isError}
           >
             <option value="all">All Cases</option>
             {[...new Set(documents.map((item) => item.caseId))].map(
@@ -261,19 +118,23 @@ const DocsPage = () => {
 
         {/* List */}
         <div className="flex w-full gap-4 flex-col">
-          {isLoading ? (
+          {isLoading && (
             <>
               <DocsCardSkeleton />
               <DocsCardSkeleton />
               <DocsCardSkeleton />
             </>
-          ) : filteredDocs.length > 0 ? (
-            filteredDocs.map((item) => <DocsCard {...item} key={item._id} />)
-          ) : (
-            <p className="text-center text-sm text-zinc-500 py-6">
-              No documents found
-            </p>
           )}
+          {isError && <ErrorMessage />}
+          {!isError &&
+            !isLoading &&
+            (filteredDocs.length > 0 ? (
+              filteredDocs.map((item) => <DocsCard {...item} key={item._id} />)
+            ) : (
+              <p className="text-center text-sm text-zinc-500 py-6">
+                No documents found
+              </p>
+            ))}
         </div>
       </div>
     </>
