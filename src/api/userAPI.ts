ff90a7d642685f1969b967c81cc32f3d980b497d
tushalbarvaliya <tushal.barvaliya@seaflux.tech>
2026-03-21 18:57:51 +0530

    feat: user API and data setup

---
+++ src/api/userAPI.ts
@@ -0,0 +1,30 @@
+import { profileData } from "@/data/userData"
+import axiosInstance from "./axiosInstance"
+import type { ProfileFormData } from "@/pages/ProfilePage"
+
+export const getProfile = async () => {
+  try {
+    const response = await axiosInstance.get("/users/profile")
+    return response.data
+  } catch {
+    return profileData
+  }
+}
+
+export const patchProfileUpdate = async (data:ProfileFormData) => {
+  try {
+    const response = await axiosInstance.patch("/users/updateProfile", data)
+    return response.data
+  } catch {
+    console.log(data)
+  }
+}
+
+export const putResetPassword = async (data) => {
+  try {
+    const response = await axiosInstance.put("/users/change_password", data)
+    return response.data
+  } catch {
+    console.log(data)
+  }
+}
