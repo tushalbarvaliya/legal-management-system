bed27c398d350f69527000899024387573605dfd
tushalbarvaliya <tushal.barvaliya@seaflux.tech>
2026-03-21 16:14:55 +0530

    feat: terms page create

--- src/pages/TermsAndConditionsPage.tsx
+++ src/pages/TermsAndConditionsPage.tsx
@@ -1,7 +1,43 @@
+import { termsAndConditionsList, type termsAndConditionsListType } from "@/utils/policyConstant"
+import { Link } from "react-router-dom"
 
 const TermsAndConditionsPage = () => {
   return (
-    <div>TermsAndConditionsPage</div>
+    <>
+      <section className="shadow-soft m-4 rounded-2xl border border-zinc-200 bg-white p-5 sm:p-6">
+        <h1 className="text-2xl font-bold tracking-tight text-zinc-900 sm:text-3xl">
+          Terms and Conditions
+        </h1>
+        <p className="mt-2 text-sm text-zinc-600 sm:text-base">
+          Last updated: March 8, 2026
+        </p>
+      </section>
+      <section className="shadow-soft m-4 flex flex-col gap-4 rounded-2xl border border-zinc-200 bg-white p-5 sm:p-6">
+        {termsAndConditionsList.map((item: termsAndConditionsListType) => (
+          <div>
+            <h2 className="text-lg font-semibold text-zinc-900">
+              {item.title}
+            </h2>
+            <p className="mt-2">{item.paragraph}</p>
+          </div>
+        ))}
+
+        <div>
+          <h2 className="text-lg font-semibold text-zinc-900">
+            9. Contact Information
+          </h2>
+          <p className="mt-2">
+            For questions about these terms, contact us at{" "}
+            <Link
+              to="mailto:privacy@acmedesk.com"
+              className="font-medium text-zinc-900 underline underline-offset-2"
+            >
+              privacy@acmedesk.com
+            </Link>
+          </p>
+        </div>
+      </section>
+    </>
   )
 }
 
