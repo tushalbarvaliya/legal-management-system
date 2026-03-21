3a8be0015bc71e76ea3e688261e622c0824ce964
tushalbarvaliya <tushal.barvaliya@seaflux.tech>
2026-03-21 14:07:21 +0530

    feat: signup api setup

--- src/api/authAPI.ts
+++ src/api/authAPI.ts
@@ -49,3 +49,40 @@ export const forgetPasswordAPI = async ({
     }
   }
 }
+
+type SignupProps = {
+  name: string
+  first_name: string
+  last_name: string
+  email: string
+  phoneNumber: string
+  password: string
+  role: string
+  gender: string
+  address: string
+  companyId: number
+}
+export type SignUpFormdata = {
+  name: string
+  first_name: string
+  last_name: string
+  email: string
+  phoneNumber: string
+  password: string
+  role: string
+  gender: string
+  address: string
+  companyId: number
+}
+
+export const signUp = async (signUpData: SignUpFormdata) => {
+  const data: SignupProps = signUpData
+  data.companyId = 1
+  try {
+    const response = await axiosInstance.post("/auth/register", data)
+    return response.data
+  } catch {
+    console.log(data)
+    return null
+  }
+}
