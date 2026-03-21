5aeadf313e9ecc7662dd7612b26b61516b4ffd70
tushalbarvaliya <tushal.barvaliya@seaflux.tech>
2026-03-21 12:01:00 +0530

    feat: login page create

--- src/api/authAPI.ts
+++ src/api/authAPI.ts
@@ -1,5 +1,6 @@
 import { loginAdminResponse, loginLawyerResponse, loginStaffResponse } from "@/data/loginData"
 import axiosInstance from "./axiosInstance"
+import { data } from "react-router-dom"
 
 type loginProps = {
   email: string
@@ -16,6 +17,7 @@ export const login = async ({ email, password }: loginProps) => {
     })
     return response.data
   } catch {
+    console.log(data);
     return loginAdminResponse
     return loginLawyerResponse
     return loginStaffResponse
