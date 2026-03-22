160b8ce4128ff53f5a48e9dbcb9a0ead9b0b4173
tushalbarvaliya <tushal.barvaliya@seaflux.tech>
2026-03-22 23:04:11 +0530

    feat: client API add

--- src/api/clientAPI.ts
+++ src/api/clientAPI.ts
@@ -1,5 +1,7 @@
 import { clientData, type ClineDataType } from "@/data/clientData"
 import axiosInstance from "./axiosInstance"
+import type { AddClientFormType } from "@/components/client/AddClientModel"
+import type { FormUpdateClient } from "@/components/client/UpdateClientModel"
 
 export const getAllClient = async () => {
   try {
@@ -10,16 +12,28 @@ export const getAllClient = async () => {
   }
 }
 
-export const postClient = async (data) => {
+type postClientProps = {
+  userId: number
+} & AddClientFormType
+
+export const postClient = async (data: postClientProps) => {
+  const clientData = {
+    ...data,
+    crNumber: 0,
+    vatNumber: 0,
+    vatPercentage: 0,
+    isDeleted: false,
+    isBlocked: false,
+  }
   try {
-    const response = await axiosInstance.post("/clients/client", data)
+    const response = await axiosInstance.post("/clients/client", clientData)
     return response.data
   } catch {
-    console.log(data)
+    console.log(clientData)
   }
 }
 
-export const patchClient = async (data) => {
+export const patchClient = async (data: FormUpdateClient) => {
   try {
     const response = await axiosInstance.patch(
       `/clients/client/${data.id}`,
@@ -31,7 +45,7 @@ export const patchClient = async (data) => {
   }
 }
 
-export const deleteClient = async (data) => {
+export const deleteClient = async (data: ClineDataType) => {
   try {
     const response = await axiosInstance.delete(`/clients/client/${data.id}`)
     return response.data
@@ -40,11 +54,11 @@ export const deleteClient = async (data) => {
   }
 }
 
-export const putBlockClient = async (data) => {
+export const putBlockClient = async (id: number) => {
   try {
-    const response = await axiosInstance.put(`/clients/client/${data.id}/block`)
+    const response = await axiosInstance.put(`/clients/client/${id}/block`)
     return response.data
   } catch {
-    console.log(`/clients/client/${data.id}/block`)
+    console.log(`/clients/client/${id}/block`)
   }
 }
