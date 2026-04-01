0a68e01ca2cdd62907678c65cdf57bf5d73a35dd
tushalbarvaliya <tushal.barvaliya@seaflux.tech>
2026-04-01 18:31:06 +0530

    style: policy page responsiveness fix

--- src/pages/TermsAndConditionsPage.tsx
+++ src/pages/TermsAndConditionsPage.tsx
@@ -1,17 +1,22 @@
 import { Link } from "react-router-dom"
 import { motion } from "framer-motion"
-import { Helmet } from 'react-helmet-async';
+import { Helmet } from "react-helmet-async"
 
 import type { PolicyType } from "@/types/types"
 import { termsAndConditionsList } from "@/utils/policyConstant"
 
 const TermsAndConditionsPage = () => {
   return (
-    <motion.div className="space-y-2" initial={{y:20,opacity:0}} animate={{y:0,opacity:1}} exit={{opacity:0,y:-20}}>
+    <motion.div
+      className="h-full space-y-2 overflow-y-scroll"
+      initial={{ y: 20, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      exit={{ opacity: 0, y: -20 }}
+    >
       <Helmet>
         <title>Terms and Conditions</title>
       </Helmet>
-      <section className="shadow-soft  rounded-2xl border border-zinc-200 bg-white p-5 sm:p-6">
+      <section className="shadow-soft rounded-2xl border border-zinc-200 bg-white p-5 sm:p-6">
         <h1 className="text-2xl font-bold tracking-tight text-zinc-900 sm:text-3xl">
           Terms and Conditions
         </h1>
@@ -19,7 +24,7 @@ const TermsAndConditionsPage = () => {
           Last updated: March 8, 2026
         </p>
       </section>
-      <section className="shadow-soft  flex flex-col gap-2 rounded-2xl border border-zinc-200 bg-white p-5 sm:p-6">
+      <section className="shadow-soft flex flex-col gap-2 rounded-2xl border border-zinc-200 bg-white p-5 sm:p-6">
         {termsAndConditionsList.map((item: PolicyType) => (
           <div key={item.title}>
             <h2 className="text-lg font-semibold text-zinc-900">
